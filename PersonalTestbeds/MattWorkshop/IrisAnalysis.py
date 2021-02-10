76a3d98ce482c7ea63a3f147c7e3d9a49faec752
mlamsey <mlamsey@gmail.com>
2021-02-10 03:13:59 +0000

    adds mean and stddev calculation for sepal length

--- PersonalTestbeds/MattWorkshop/IrisAnalysis.py
+++ PersonalTestbeds/MattWorkshop/IrisAnalysis.py
@@ -1,20 +1,20 @@
-# self.species.where(self.species == "Iris-setosa").dropna();
-
 import pandas as pd;
 import numpy as np;
 
 class IrisAnalysis:
+    n_round = 3;
 
     @staticmethod
     def Analyze():
         setosa, versicolor, virginica = IrisAnalysis.__LoadRawData();
+        IrisAnalysis.__CalculateSepalLengthMeans(setosa, versicolor, virginica);
 
     @staticmethod
     def __LoadRawData():
         raw_data = pd.read_csv("DataSets/Iris/Iris.csv");
 
         setosa_data = raw_data.loc[raw_data.Species == "Iris-setosa"];
-        versicolor_data = raw_data.loc[raw_data.Species == "Iris-versicolor_data"];
+        versicolor_data = raw_data.loc[raw_data.Species == "Iris-versicolor"];
         virginica_data = raw_data.loc[raw_data.Species == "Iris-virginica"];
 
         setosa_objects = IrisAnalysis.__GenerateIrisDataSubset(setosa_data);
@@ -28,7 +28,29 @@ class IrisAnalysis:
     def __GenerateIrisDataSubset(dataframe):
         subset = [];
         for index, row in dataframe.iterrows():
-            subset.append(_IrisData(row["Id"],row["SepalLengthCm"],row["SepalWidthCm"],row["PetalLengthCm"],row["PetalWidthCm"],row["Species"]))
+            subset.append(_IrisData(row["Id"],row["SepalLengthCm"],row["SepalWidthCm"],row["PetalLengthCm"],row["PetalWidthCm"],row["Species"]));
+
+        return subset;
+
+    @staticmethod
+    def __CalculateSepalLengthMeans(setosa, versicolor, virginica):
+        setosa_sepal_lengths = [iris.sepal_length for iris in setosa];
+        versicolor_sepal_lengths = [iris.sepal_length for iris in versicolor];
+        virginica_sepal_lengths = [iris.sepal_length for iris in virginica];
+
+        setosa_sepal_length_mean = np.mean(setosa_sepal_lengths)
+        setosa_sepal_length_std = np.std(setosa_sepal_lengths)
+        print("Setosa Mean Sepal Length: " + str(round(setosa_sepal_length_mean,IrisAnalysis.n_round)) + " | Setosa Stddev Sepal Length: " + str(round(setosa_sepal_length_std,IrisAnalysis.n_round)))
+
+        versicolor_sepal_length_mean = np.mean(versicolor_sepal_lengths)
+        versicolor_sepal_length_std = np.std(versicolor_sepal_lengths)
+        print("Versicolor Mean Sepal Length: " + str(round(versicolor_sepal_length_mean,IrisAnalysis.n_round)) + " | Versicolor Stddev Sepal Length: " + str(round(versicolor_sepal_length_std,IrisAnalysis.n_round)))
+
+        virginica_sepal_length_mean = np.mean(virginica_sepal_lengths)
+        virginica_sepal_length_std = np.std(virginica_sepal_lengths)
+        print("Virginica Mean Sepal Length: " + str(round(virginica_sepal_length_mean,IrisAnalysis.n_round)) + " | Virginica Stddev Sepal Length: " + str(round(virginica_sepal_length_std,IrisAnalysis.n_round)))
+        
+        return setosa_sepal_length_mean, versicolor_sepal_length_mean, virginica_sepal_length_mean
 
 # Private Classes
 class _IrisData:
